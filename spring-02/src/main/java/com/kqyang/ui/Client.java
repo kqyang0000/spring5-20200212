fcbb594c8a5e10e2c023093208689a97a65edd01
kqyang <kqyang0000@163.com>
2020-02-17 19:11:25 +0800

    ApplicationContext与BeanFactory的创建bean区别

--- spring-02/src/main/java/com/kqyang/ui/Client.java
+++ spring-02/src/main/java/com/kqyang/ui/Client.java
@@ -5,14 +5,31 @@ import com.kqyang.service.IAccountService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Map;
+
 public class Client {
 
+    /**
+     * ApplicationContext：
+     *      AnnotationConfigApplicationContext
+     *      ClassPathXmlApplicationContext
+     *      FileSystemXmlApplicationContext
+     * <p>
+     * 两个核心容器引发的问题：
+     *      单例
+     *      ApplicationContext:
+     *          在构造核心容器时，创建对象采用立即加载的方式。也即是说，读取完配置文件后，就马上创建配置文件中的对象
+     *      多例
+     *      BeanFactory:
+     *          在构造核心容器时，创建对象采用延迟加载的方式。也就是说，什么时候根据id获取对象了，什么时候才真正的创建对象
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
         IAccountService accountService = (IAccountService) applicationContext.getBean("accountService");
         IAccountDao accountDao = applicationContext.getBean("accountDao", IAccountDao.class);
         System.out.println(accountService);
         System.out.println(accountDao);
-
     }
 }
