43a65043770744a15699e4d18115ed886487db1d
kqyang <kqyang0000@163.com>
2020-03-04 23:40:53 +0800

    基于配置文件的aop切面编程

---
+++ spring-11/src/main/java/com/kqyang/service/IAccountService.java
@@ -0,0 +1,9 @@
+package com.kqyang.service;
+
+public interface IAccountService {
+    void saveAccount();
+
+    void updateAccount(int i);
+
+    int deleteAccount();
+}
---
+++ spring-11/src/main/java/com/kqyang/utils/Logger.java
@@ -0,0 +1,7 @@
+package com.kqyang.utils;
+
+public class Logger {
+    public void printLog() {
+        System.out.println("Logger类型的pringLog方法开始记录日志");
+    }
+}
